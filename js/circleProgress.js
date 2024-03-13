137a37e99120aa298d4bf61c44792ba933c8b59b
cn666278 <cst1909128@xmu.edu.my>
2024-03-13 11:53:27 +0000

    add progress circle

---
+++ js/circleProgress.js
@@ -0,0 +1,23 @@
+function setProgress(elem, percent) {
+    var
+      degrees = percent * 3.6,
+      transform = /MSIE 9/.test(navigator.userAgent) ? 'msTransform' : 'transform';
+    elem.querySelector('.counter').setAttribute('data-percent', Math.round(percent));
+    elem.querySelector('.progressEnd').style[transform] = 'rotate(' + degrees + 'deg)';
+    elem.querySelector('.progress').style[transform] = 'rotate(' + degrees + 'deg)';
+    if(percent >= 50 && !/(^|\s)fiftyPlus(\s|$)/.test(elem.className))
+      elem.className += ' fiftyPlus';
+  }
+ 
+ 
+const e = (elem, start, end) => {
+    start += 1;
+    setProgress(elem, (start));
+    if(start < end) {
+        setTimeout(function() { e(elem, start, end); }, 5);
+    }
+}
+ 
+function start() {
+    e(document.getElementById('ee'), 0, 72)
+}
