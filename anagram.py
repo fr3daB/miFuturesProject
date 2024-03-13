4e60ba137e657b9d26e1605fa1f3c59e992eea87
C23002705 <bulemf@cardiff.ac.uk>
2024-03-13 11:31:23 +0000

    finished puzzle

--- anagram.py
+++ anagram.py
@@ -1,9 +1,11 @@
-from hangman import *
 import random
 
+
 def anagram(word_list):
+    tries = 0
     
     word_index = random.randint(0,len(word_list)-1)
+    theWord = word_list[word_index]
 
     letter_list = list(word_list[word_index])
     word_length = len(letter_list)
@@ -14,7 +16,29 @@ def anagram(word_list):
 
         letter_list[index1], letter_list[index2] = letter_list[index2], letter_list[index1]
 
-    return "".join(letter_list)
+
+    if "".join(letter_list) not in word_list:
+        print( "".join(letter_list)) 
+    else:
+        print( anagram(word_list))
+
+    response = input("Enter the original word! :> ")
+    tries += 1
+    while response != theWord:
+        print("Please reattempt! ")
+        tries += 1
+        response = input("Enter the original word! :> ")
+    print("SUCCESS! SOLVED IN " + str(tries) + " TRIES!")
 
 
 
+
+
+
+easywords = ["river", "table", "happy", "guitar", "movie"]
+mediumwords = ["elephant", "computer", "library", "pancake", "journey"]
+intermediatewords = [
+    "persevere", "ethereal", "enigma", "quagmire", "insidious"]
+hardwords = ["serendipity", "exquisite", "cacophany", "mellifluos", "quixotic"]
+
+#anagram(easywords)
