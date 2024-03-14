73d63c01c1a3b5a020122aabb6a2811189d0d402
Oscar Hoodsmith <oscarhoodsmith@gmail.com>
2024-03-14 10:40:51 +0000

    linked games to level page

--- js/level.js
+++ js/level.js
@@ -19,5 +19,45 @@ const e = (elem, start, end) => {
 }
 
 function start() {
-    e(document.getElementById('c1'), 0, 72)
+  // Reset
+  localStorage.setItem("l1percent", 0);
+  localStorage.setItem("l1", 1);
+  console.log(localStorage.getItem("l1percent"));
+  e(document.getElementById('c1'), 0, localStorage.getItem("l1percent"));
+  document.getElementById('lebar').style.width = `${localStorage.getItem("l1percent")/document.getElementById('lebar').style.width}px`;
+}
+
+
+function select1() {
+  var l = localStorage.getItem('l1');
+  console.log(l);
+  switch (l) {
+    case "1":
+      localStorage.setItem("mc", 1);
+      localStorage.setItem("l1percent", 20);
+      location.href = '../html/multipleChoice.html';
+      break;
+    case "2":
+      localStorage.setItem("md", 1);
+      localStorage.setItem("l1percent", 40);
+      location.href = '../html/memoryGame.html';
+      break;
+    case "3":
+      localStorage.setItem("md", 2);
+      localStorage.setItem("l1percent", 60);
+      location.href = '../html/memoryGame.html';
+      break;
+    case "4":
+      localStorage.setItem("mc", 2);
+      localStorage.setItem("l1percent", 80);
+      location.href = '../html/multipleChoice.html';
+      break;
+    case "5":
+      localStorage.setItem("md", 3);
+      localStorage.setItem("l1percent", 100);
+      location.href = '../html/memoryGame.html';
+        break;
+    default:
+      location.href = '../html/skillPage.html';
+  }
 }
